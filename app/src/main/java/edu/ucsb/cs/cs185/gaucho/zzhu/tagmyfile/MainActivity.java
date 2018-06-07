2f62b23bc5287c5ab33cbd9fbc99ac5165796021
Scott Zhu <scottzhu1996@live.com>
2018-06-06 22:11:35 -0700

    RecyclerView Finished

--- app/src/main/java/edu/ucsb/cs/cs185/gaucho/zzhu/tagmyfile/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/gaucho/zzhu/tagmyfile/MainActivity.java
@@ -4,12 +4,23 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private void initData(List<MyFile> files) {
+        files = new ArrayList<>();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        RecyclerView recView = (RecyclerView) findViewById(R.id.recView);
+        ArrayList<MyFile> files = new ArrayList<>();
+        files.add(new MyFile("CS 185 HW1",
+                "Just another homework",
+                "",
+                new HashSet<String>(Arrays.asList("pdf", "cs185", "hci", "nofile")),
+                R.drawable.ic_insert_drive_file_black_24dp));
+        files.add(new MyFile("CS 185 HW2",
+                "Just another homework",
+                "",
+                new HashSet<String>(Arrays.asList("folder", "cs185", "hci", "nofile")),
+                R.drawable.photo));
+
+        ViewAdapter adapter = new ViewAdapter(files);
+        recView.setAdapter(adapter);
+        LinearLayoutManager llm = new LinearLayoutManager(null);
+        recView.setLayoutManager(llm);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/edu/ucsb/cs/cs185/gaucho/zzhu/tagmyfile/ViewAdapter.java
+++ app/src/main/java/edu/ucsb/cs/cs185/gaucho/zzhu/tagmyfile/ViewAdapter.java
@@ -2,30 +2,61 @@ package edu.ucsb.cs.cs185.gaucho.zzhu.tagmyfile;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class MyFile {
+    String name;
+    String description;
+    String path;
+    Set<String> tagList;
+    Integer photoId;
+
+    MyFile(String name, String description, String path, Set<String> tagList, Integer photoId) {
+        this.name = name;
+        this.description = description;
+        this.path =path;
+        this.tagList = tagList;
+        this.photoId = photoId;
+    }
+}
+
 public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.PersonViewHolder>{
 
-    ViewAdapter(){
 
+    private List<MyFile> files;
+
+    ViewAdapter(List<MyFile> files){
+        this.files = files;
+        Log.d("fi", Integer.toString(getItemCount()));
     }
 
     @Override
     public ViewAdapter.PersonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
+        PersonViewHolder pvh = new PersonViewHolder(v);
+        return pvh;
     }
 
     @Override
     public void onBindViewHolder(ViewAdapter.PersonViewHolder holder, int position) {
-
+        holder.fileName.setText(files.get(position).name);
+        holder.iconPhoto.setImageResource(files.get(position).photoId);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return files.size();
     }
 
     public class PersonViewHolder extends RecyclerView.ViewHolder {
